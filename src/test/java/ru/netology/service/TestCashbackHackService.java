69bfcd228b092a6a5b2218e8a77740a5ef5705d5
saphess <saphess@mail.ru>
2024-10-25 11:31:03 +0300

    Tests junit4

--- src/test/java/ru/netology/service/TestCashbackHackService.java
+++ src/test/java/ru/netology/service/TestCashbackHackService.java
@@ -1,7 +1,7 @@
 package ru.netology.service;
 
-import org.testng.Assert;
-import org.testng.annotations.Test;
+import org.junit.Assert;
+import org.junit.Test;
 
 public class TestCashbackHackService {
     @Test
@@ -11,7 +11,7 @@ public class TestCashbackHackService {
         int actual = cashbackHackService.remain(1);
         int expect = 999;
 
-        Assert.assertEquals(actual, expect);
+        Assert.assertEquals(expect, actual);
     }
 
     @Test
@@ -19,9 +19,9 @@ public class TestCashbackHackService {
         CashbackHackService cashbackHackService = new CashbackHackService();
 
         int actual = cashbackHackService.remain(0);
-        int expect = 1000;
+        int expect = 0;
 
-        Assert.assertEquals(actual, expect);
+        Assert.assertEquals(expect, actual);
     }
     @Test
     public void shouldRemainIfAmountIs500(){
@@ -30,7 +30,7 @@ public class TestCashbackHackService {
         int actual = cashbackHackService.remain(500);
         int expect = 500;
 
-        Assert.assertEquals(actual, expect);
+        Assert.assertEquals(expect, actual);
     }
     @Test
     public void shouldRemainIfAmountIs1001(){
@@ -39,15 +39,6 @@ public class TestCashbackHackService {
         int actual = cashbackHackService.remain(1001);
         int expect = 999;
 
-        Assert.assertEquals(actual, expect);
-    }
-    @Test
-    public void shouldRemainIfAmountIs1000(){
-        CashbackHackService cashbackHackService = new CashbackHackService();
-
-        int actual = cashbackHackService.remain(1000);
-        int expect = 1000;
-
-        Assert.assertEquals(actual, expect);
+        Assert.assertEquals(expect, actual);
     }
 }
